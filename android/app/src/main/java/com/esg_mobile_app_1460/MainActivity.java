1f0f356ceb780ea2240075262247e471db56ae88
Crowdbotics <team@crowdbotics.com>
2019-03-18 18:01:45 +0000

    initial commit

---
+++ android/app/src/main/java/com/esg_mobile_app_1460/MainActivity.java
@@ -0,0 +1,15 @@
+package com.esg_mobile_app_1460;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "esg_mobile_app_1460";
+    }
+}
